704424664df1bdd560649ce6c486ec91d2b26f38
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-07-05 16:46:52 -0400

    Person Class Bean is Created

---
+++ src/com/pavan/yalamanchili/Person.java
@@ -0,0 +1,15 @@
+package com.pavan.yalamanchili;
+
+public class Person {
+
+	public void setFirstName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
